f7bdcf5afe4c5118b659ad5c9c701c5ad84b978e
Dudás Tamás Alex <99723567+DuT0mi@users.noreply.github.com>
2022-07-29 12:27:01 +0200

    Create lcode2011.java

---
+++ lcode2011.java
@@ -0,0 +1,21 @@
+/**
+*   2011. Final Value of Variable After Performing Operations
+* /   
+public class Leetcode {
+
+        public static int finalValueAfterOperations(String[] operations) {
+                int init = 0;
+                for(String param:operations)
+                      if(param.equals("X++") || param.equals("++X"))
+                              init++;
+                        else
+                                init--;
+                return  init;
+        }
+        public static  void main(String[] args){
+                String []operations = {"++X","X++","X++"};
+                int value = finalValueAfterOperations(operations);
+                System.out.println("The value is: " + value);
+        }
+
+}
