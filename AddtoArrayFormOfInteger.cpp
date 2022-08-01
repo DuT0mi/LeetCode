e1f693312106f1d900629182ba95345ab05394bd
Dudás Tamás Alex <99723567+DuT0mi@users.noreply.github.com>
2022-08-01 17:03:42 +0200

    Create AddtoArrayFormOfInteger.cpp

---
+++ AddtoArrayFormOfInteger.cpp
@@ -0,0 +1,13 @@
+// [Easy]
+    vector<int> addToArrayForm(vector<int>& num, int k) {
+        for(int i = num.size() - 1; i >= 0 && k > 0; i--) {
+            num[i] += k;
+            k = num[i] / 10;
+            num[i] %= 10;
+        }
+        while(k > 0) {
+            // Add element to the front
+            num.insert(num.begin(), k % 10); k /= 10;
+        }
+        return num;
+   }
