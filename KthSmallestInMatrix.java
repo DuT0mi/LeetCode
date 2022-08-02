869591340af6919654f7eb9c66353651a1754820
Dudás Tamás Alex <99723567+DuT0mi@users.noreply.github.com>
2022-08-02 15:33:39 +0200

    Create KthSmallestInMatrix.java

---
+++ KthSmallestInMatrix.java
@@ -0,0 +1,17 @@
+// [Medium]
+class Solution {
+    // bruteforce but it's time complexity is smaller than O(n^2)
+    public int kthSmallest(int[][] matrix, int k) {
+        int n = matrix.length;        // getting the x's dimension
+        int m = matrix[0].length;     // getting the y's dimension
+        int[] arr = new int[n * m];
+        int ki=0;
+        for(int i =0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                arr[ki++] = matrix[i][j];
+            }
+        }
+        Arrays.sort(arr);
+        return arr[k-1];
+    }
+}
